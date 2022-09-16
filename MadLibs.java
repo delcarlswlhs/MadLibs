e7d2deb6391be04f63803adc7565bf3bd296241e
delcarlswlhs <113708789+delcarlswlhs@users.noreply.github.com>
2022-09-16 09:27:07 -0700

    Add files via upload

---
+++ MadLibs.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+/**
+ * Fall MadLibs
+ * Simone Del Carlo
+ * 9/16/22
+ */
+public class MadLibs
+{
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter your name: ");
+       String userName = input.nextLine();
+       
+       System.out.println("Enter the month: ");
+       int month = input.nextInt();
+       System.out.println("Enter the day: ");
+       int day = input.nextInt();
+       System.out.println("Enter the year: ");
+       int year = input.nextInt();
+       
+       input.nextLine();
+       
+       System.out.println("Enter a noun: ");
+       String firstNoun = input.nextLine();
+       System.out.println("Enter a color: ");
+       String firstColor = input.nextLine();
+       System.out.println("Enter a color: ");
+       String secondColor = input.nextLine();
+       System.out.println("Enter a noun: ");
+       String secondNoun = input.nextLine();
+       System.out.println("Enter an adjective: ");
+       String firstAdj = input.nextLine();
+       System.out.println("Enter a noun: ");
+       String thirdNoun = input.nextLine();
+       System.out.println("Enter a vegetable: ");
+       String firstVeg = input.nextLine();
+       System.out.println("Enter a person: ");
+       String firstPerson = input.nextLine();
+       System.out.println("Enter a verb: ");
+       String firstVerb = input.nextLine();
+       System.out.println("Enter an adjective: ");
+       String secondAdj = input.nextLine();
+       System.out.println("Enter a vegetable: ");
+       String secondVeg = input.nextLine();
+       System.out.println("Enter an adjective: ");
+       String thirdAdj = input.nextLine();
+       System.out.println("Enter a vegetable: ");
+       String thirdVeg = input.nextLine();
+       System.out.println("Enter a noun: ");
+       String fourthNoun = input.nextLine();
+       
+       
+   }
+}
